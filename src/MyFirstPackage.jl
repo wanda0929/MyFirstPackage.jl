d4f7dc349dc2299cc73950a85a057ddd993e3701
hanwang <hanwang@10.0.179>
2024-02-28 19:26:45 +0800

    initial

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -1,5 +1,13 @@
 module MyFirstPackage
+# import packages
+using LinearAlgebra
 
-# Write your package code here.
+# export interfaces
+export Lorenz, integrate_step
+export Point, Point2D, Point3D
+export RungeKutta, Euclidean
+
+# `include` other source files into this module
+include("lorenz.jl")
 
 end
